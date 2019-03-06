5e68e370507eda30c958d191c717abba21e78a4a
aneesh-mj <48265776+aneesh-mj@users.noreply.github.com>
2019-03-06 06:48:12 +0530

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test: /\.html$/,
+                use: [
+                    {
+                        loader: "html-loader"
+                    }
+                ]
+            }
+        ]
+    },
+    devServer: {
+        port: 3100,
+        open: true,
+        proxy: {
+            "/api": "http://localhost:8080"
+        }
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: "./src/index.html",
+            filename: "./index.html"
+        })
+    ]
+};
